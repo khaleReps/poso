ec07cae1c43efda1dd4910732a78912ec590c3b4
Khotso <khotso@gitlab.instacom.co.za>
2023-07-11 11:31:35 +0200

    Removed Django

--- POSO/mailing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from mailing import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('send_email/', views.send_email, name='send_email'),
-]
--- POSO/users/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class EmailLoginSettingsForm(forms.Form):
-    email_host = forms.CharField(label='Email Host')
-    email_port = forms.IntegerField(label='Email Port')
-    email_use_ssl = forms.BooleanField(label='Use SSL', required=False)
